1d9365ce43ee24ca5965be339295b9113605e8de
VanLinh <phamvanlinh9b.lhp@gmail.com>
2023-02-09 21:46:26 +0700

    login

---
+++ ShopProjectAsp_PhamVanLinh/Models/CategoryProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopProjectAsp_PhamVanLinh.Models
+{
+    public class CategoryProductViewModel
+    {
+        public Product products { get; set; }
+        public Category categorys { get; set; }
+    }
+}
